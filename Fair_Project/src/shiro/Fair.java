caba435f22531e5b192d3b4f7599ed005d6e9405
Shiro <jaron.95@hotmail.com>
2021-06-02 16:57:32 +0200

    Fair Project

---
+++ Fair_Project/src/shiro/Fair.java
@@ -0,0 +1,20 @@
+package shiro;
+
+import java.util.Scanner;
+
+public class Fair {
+
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print(("Type your number"));
+		int input = sc.nextInt();
+		System.out.print((input));
+		if(input == 4 ) {
+			sc.close();
+		}
+	}
+
+}
